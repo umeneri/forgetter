516f466f4b2f33a9779a610d1de5c9272ee4391b
umeneri <koishimst@gmail.com>
2019-05-12 19:14:47 +0900

    init akka example

---
+++ src/main/scala/com/example/JsonSupport.scala
@@ -0,0 +1,16 @@
+package com.example
+
+import com.example.UserRegistryActor.ActionPerformed
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json.DefaultJsonProtocol
+
+trait JsonSupport extends SprayJsonSupport {
+  // import the default encoders for primitive types (Int, String, Lists etc)
+  import DefaultJsonProtocol._
+
+  implicit val userJsonFormat = jsonFormat3(User)
+  implicit val usersJsonFormat = jsonFormat1(Users)
+
+  implicit val actionPerformedJsonFormat = jsonFormat1(ActionPerformed)
+}
